b92fe49f280763954084a945c71106d5075eabd9
wosen7 <wosen7>
2010-03-19 10:47:51 +0000

    added isomap

---
+++ src/isomap.h
@@ -0,0 +1,41 @@
+/***************************************************************************
+ *   Copyright (C) 2010 by Matthias Ihrke                                  *
+ *   ihrke@nld.ds.mpg.de
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   aint with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+/**\file isomap.h
+ \brief \ref status_inprogress isomap
+
+isomap
+	
+ */
+#ifndef ISOMAP_H
+# define ISOMAP_H
+#include "definitions.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  /* -------------- FUNCTIONS ---------------- */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* isomap_H */
